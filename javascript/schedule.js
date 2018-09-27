397a3b5751ddb08d2072c316eb7be02b79a89b4b
NBCorrigan <nickb.corrigan@gmail.com>
2018-09-27 16:40:44 -0700

    updates

--- javascript/schedule.js
+++ javascript/schedule.js
@@ -1,100 +1,95 @@
-$(document).ready(function() {
-
-	// 1. Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyA44X5SXFjpGs0BRaFVoTWgo3pijXhG7jg",
-        authDomain: "trainschedule-708f2.firebaseapp.com",
-        databaseURL: "https://trainschedule-708f2.firebaseio.com",
-        projectId: "trainschedule-708f2",
-        storageBucket: "",
-        messagingSenderId: "840055368345"
-      };
-  
+//firebase initialization
+var config = {
+    apiKey: "AIzaSyA44X5SXFjpGs0BRaFVoTWgo3pijXhG7jg",
+    authDomain: "trainschedule-708f2.firebaseapp.com",
+    databaseURL: "https://trainschedule-708f2.firebaseio.com",
+    projectId: "trainschedule-708f2",
+    storageBucket: "trainschedule-708f2.appspot.com",
+    messagingSenderId: "840055368345"
+  };
   firebase.initializeApp(config);
-
+  
+  // Create a variable to reference the database.
   var database = firebase.database();
-
- 
-
-  // First Time (pushed back 1 year to make sure it comes before current time)
-   
-  // 2. Button for adding Trains
-  $("#add-train-btn").on("click", function(event) {
-  		event.preventDefault();
-
-	 // Grabs user input
-	  var trainName = $("#train-name-input").val().trim();
-	  var trainDest = $("#dest-input").val().trim();
-	  var firstTrain = $("#firstTrain-input").val().trim();
-	  var trainFreq = $("#freq-input").val().trim();
-
-	  // Creates local "temporary" object for holding train data
+  
+  // -----------------------------
+  
+  //button to add new trains
+  $('#newTrainBtn').on('click', function(){
+  
+	var trainName = $('#trainNameInput').val().trim();
+	  var destination = $('#destInput').val().trim();
+	  var firstTrain = $('#firstTrainInput').val().trim();
+	  var frequency = $('#freqInput').val().trim();
+  
+	// Creates local "temporary" object for holding train data
 	  var newTrain = {
-	  	name: trainName,
-	  	destination: trainDest,
-	  	start: firstTrain,
-	  	frequency: trainFreq
-	  };
+		  name: trainName,
+		  dest: destination,
+		  first: firstTrain,
+		  freq: frequency
+	  }
+  		
+	//Uploads employee data to the database
+	  database.ref().push(newTrain);
+  
+	  console.log(newTrain.name);
+	  console.log(newTrain.dest);
+	  console.log(newTrain.first);
+	  console.log(newTrain.freq);
+	
 
-	  // Uploads train data to the database
-  		database.ref().push(newTrain);
-
-
-	   // Alert
-  		alert("Train successfully added");
-
-	 // Clears all of the text-boxes
-	  $("#train-name-input").val("");
-	  $("#dest-input").val("");
-	  $("#firstTrain-input").val("");
-	  $("#freq-input").val("");
-  	});
-
-  	// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
-	database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
+	  
+	// Clears all of the text-boxes
+	  $('#trainNameInput').val("");
+	  $('#destInput').val("");
+	  $('#firstTrainInput').val("");
+	  $('#freqInput').val("");
+  
+  return false;
+  })
+  
+  // Creates a Firebase event for adding trains to the database and a row in the html
+  database.ref().on("child_added", function(childSnapshot){
 	  console.log(childSnapshot.val());
-
-	  // Store everything into a variable.
+  
+	  // Store everything into a variable
 	  var trainName = childSnapshot.val().name;
-	  var trainDest = childSnapshot.val().destination;
-	  var firstTrain = childSnapshot.val().start;
-	  var trainFreq = childSnapshot.val().frequency;
-
-
-	   // Declare variable
-  		var trainFreq;
-
-  		// Time is to be entered on the entry form
-   		 var firstTime = 0;
-
-	   var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-	    console.log(firstTimeConverted);
-
-	  // Current Time
-	    var currentTime = moment();
-	    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
-
-	  // Difference between the times
-		var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-		console.log("DIFFERENCE IN TIME: " + diffTime);
-
-		// Time apart (remainder)
-	    var tRemainder = diffTime % trainFreq;
-	    console.log(tRemainder);
-
-	    // Minute Until Train
-	    var tMinutesTillTrain = trainFreq - tRemainder;
-	    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-	    // Next Train
-	    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-	    console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
-
-
-	  // Add each train's data into the table
-	  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + 
-	   "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");
-	});
-
-});
+	  var destination = childSnapshot.val().dest;
+	  var firstTrain = childSnapshot.val().first;
+	  var frequency = childSnapshot.val().freq;
+  
+	  // Train info
+	  console.log(trainName);
+	  console.log(destination);
+	  console.log(firstTrain);
+	  console.log(frequency);
+  
+	  //First time
+	  var firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+	  console.log(firstTimeConverted);
+  
+	  // Current time
+	  var currentTime = moment();
+	  console.log("CURRENT TIME:" + moment(currentTime).format("HH:mm"));
+  
+	  // Difference between times
+	  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	  console.log("DIFFERENCE IN TIME: " + diffTime);
+  
+	  // Time apart (remainder)
+	  var tRemainder = diffTime % frequency;
+	  console.log(tRemainder);
+  
+	  // Mins until train
+	  var tMinutesTillTrain = frequency - tRemainder;
+	  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+  
+	  // Next train
+	  var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+	  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  
+  
+	  $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination  + "</td><td>" + frequency + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>");
+  
+  });
